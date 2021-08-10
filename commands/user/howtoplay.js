36531c7137cee14d9f6d456513bdf396bcb8f2e6
Jassieboop <jasbosua@gmail.com>
2021-08-10 12:34:23 +0200

    Update ^howtoplay for testing

--- commands/user/howtoplay.js
+++ commands/user/howtoplay.js
@@ -80,59 +80,58 @@ module.exports = {
 				btnLast
 			);
 
-		let current = [];
+		let current;
 
 		let page = 0;
 
 		current.push(mainpage);
 
-		message.channel.send({ embeds: [current[0]], components: [row1, row2] }).then(async (m) => {
-			const filter = async (interaction) => {
-				if (interaction.user.id === message.author.id) {
-					return true;
-				} else {
-					interaction.followUp({ content: "This message isn't for you" });
-				}
-			};
+		const msg = message.channel.send({ embeds: [current[0]], components: [row1, row2] })
+		const filter = async (interaction) => {
+			if (interaction.user.id === message.author.id) {
+				return true;
+			} else {
+				interaction.followUp({ content: "This message isn't for you" });
+			}
+		};
 
-			const collector = m.createMessageComponentCollector({ filter, idle: 20000 });
+		const collector = msg.createMessageComponentCollector({ filter, idle: 20000 });
 
-			collector.on('collect', async (interaction, user) => {
-				interaction.deferUpdate();
+		collector.on('collect', async (interaction, user) => {
+			interaction.deferUpdate();
 
-				if (interaction.isButton()) {
+			if (interaction.isButton()) {
 
-					switch (interaction.customId) {
+				switch (interaction.customId) {
 
-						case '1':
-							page = 0;
-							return m.edit({ embeds: [current[page]] });
-						case '2':
-							return m.edit({ embeds: [current[--page]] });
-						case '3':
-							return m.edit({ embeds: [current[++page]] });
-						case '4':
-							page = current.length - 1;
-							return m.edit({ embeds: [current[page]] });
-
-					}
-
-				} else if (interaction.isSelectMenu()) {
-
-					if (interaction.values[0] == 'character') {
-
-						current = characterEmbeds;
-						m.edit({ embeds: [current[0]] });
-
-					} else if (interaction.values[0] == 'test-list2') {
-
-						current
-						m.edit({ embeds: [current[0]] });
-
-					}
+					case '1':
+						page = 0;
+						return m.edit({ embeds: [current[page]] });
+					case '2':
+						return m.edit({ embeds: [current[--page]] });
+					case '3':
+						return m.edit({ embeds: [current[++page]] });
+					case '4':
+						page = current.length - 1;
+						return m.edit({ embeds: [current[page]] });
 
 				}
-			});
+
+			} else if (interaction.isSelectMenu()) {
+
+				if (interaction.values[0] == 'character') {
+
+					current = characterEmbeds;
+					m.edit({ embeds: [current[0]] });
+
+				} else if (interaction.values[0] == 'test-list2') {
+
+					current
+					m.edit({ embeds: [current[0]] });
+
+				}
+
+			}
 		});
 	}
 };
